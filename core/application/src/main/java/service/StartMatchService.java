336906e8c02073fdab8c45aaa5c650b427ef4d61
Grzegorz Pelc <grzegorz.pelc.fy@gmail.com>
2025-07-02 20:42:06 +0200

    Implement StartMatchService

--- core/application/src/main/java/service/StartMatchService.java
+++ core/application/src/main/java/service/StartMatchService.java
@@ -4,9 +4,16 @@ import port.in.StartMatchUseCase;
 import port.out.StartMatchPort;
 
 public class StartMatchService implements StartMatchUseCase {
+
+    private final StartMatchPort startMatchPort;
+
+    public StartMatchService(StartMatchPort startMatchPort) {
+        this.startMatchPort = startMatchPort;
+    }
+
     @Override
     public void apply(String matchData) {
-
+        startMatchPort.apply(matchData);
     }
 }
 
