35a973ed66b050119fbb62cb03ddd87a115efd7d
Grzegorz Pelc <grzegorz.pelc.fy@gmail.com>
2025-07-04 16:17:14 +0200

    add needed attributes to FootballMatch and generate getters and setters

--- core/domain/src/main/java/com/scoreboard/core/domain/FootballMatch.java
+++ core/domain/src/main/java/com/scoreboard/core/domain/FootballMatch.java
@@ -1,16 +1,68 @@
 package com.scoreboard.core.domain;
 
+import java.time.Instant;
+
 public class FootballMatch {
 
-    public String homeTeam;
-    public String awayTeam;
-    public int homeTeamScore;
-    public int awayTeamScore;
+    private String homeTeam;
+    private String awayTeam;
+    private int homeTeamScore;
+    private int awayTeamScore;
+    private Instant startedAt;
+    private Instant finishedAt;
 
     public FootballMatch (String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore) {
         this.homeTeam = homeTeam;
         this.awayTeam = awayTeam;
         this.homeTeamScore = homeTeamScore;
         this.awayTeamScore = awayTeamScore;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public int getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+    public Instant getStartedAt() {
+        return startedAt;
+    }
+
+    public Instant getFinishedAt() {
+        return finishedAt;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public void setHomeTeamScore(int homeTeamScore) {
+        this.homeTeamScore = homeTeamScore;
+    }
+
+    public void setAwayTeamScore(int awayTeamScore) {
+        this.awayTeamScore = awayTeamScore;
+    }
+
+    public void setStartedAt(Instant startedAt) {
+        this.startedAt = startedAt;
+    }
+
+    public void setFinishedAt(Instant finishedAt) {
+        this.finishedAt = finishedAt;
     }
 }
