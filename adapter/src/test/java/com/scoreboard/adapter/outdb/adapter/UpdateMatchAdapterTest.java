19ed5066d2c99ecbc31c535b88cf645d8499c1ca
Grzegorz Pelc <grzegorz.pelc.fy@gmail.com>
2025-07-06 04:19:02 +0200

    write unit test for UpdateMatchAdapter

---
+++ adapter/src/test/java/com/scoreboard/adapter/outdb/adapter/UpdateMatchAdapterTest.java
@@ -0,0 +1,49 @@
+package com.scoreboard.adapter.outdb.adapter;
+
+import com.scoreboard.adapter.outdb.entity.FootballMatchEntity;
+import com.scoreboard.adapter.outdb.repository.ScoreboardRepository;
+import com.scoreboard.core.domain.FootballMatch;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UpdateMatchAdapterTest {
+
+    static ScoreboardRepository scoreboardRepository;
+
+    private static UpdateMatchAdapter adapterUnderTest;
+
+    @BeforeAll
+    static void setUpDb() {
+        adapterUnderTest = new UpdateMatchAdapter();
+        scoreboardRepository = ScoreboardRepository.getInstance();
+        scoreboardRepository.setupTestData();
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        scoreboardRepository.cleanUpDb();
+    }
+
+    @Test
+    void shouldUpdateMatch() {
+        //given
+        String homeTeam = "Germany";
+        String awayTeam = "France";
+        FootballMatch footballMatch = new FootballMatch(homeTeam, awayTeam, 3, 1);
+        //when
+        adapterUnderTest.apply(footballMatch);
+        //then
+        FootballMatchEntity result = scoreboardRepository.getSummary().stream()
+                .filter(matchEntity -> matchEntity.getHomeTeam().equals(homeTeam) && matchEntity.getAwayTeam().equals(awayTeam))
+                .findFirst()
+                .orElse(null);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getHomeTeamScore()).isEqualTo(3);
+        assertThat(result.getAwayTeamScore()).isEqualTo(1);
+        assertThat(result.getAwayTeamLastScore()).isNotNull();
+    }
+}
