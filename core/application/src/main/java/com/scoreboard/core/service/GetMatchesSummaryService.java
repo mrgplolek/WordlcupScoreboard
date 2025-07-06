b4d988b1654ca1c66cd7d37a54f438bf2e4aa5c0
Grzegorz Pelc <grzegorz.pelc.fy@gmail.com>
2025-07-06 20:47:21 +0200

    implement GetMatchesSummaryService

--- core/application/src/main/java/com/scoreboard/core/service/GetMatchesSummaryService.java
+++ core/application/src/main/java/com/scoreboard/core/service/GetMatchesSummaryService.java
@@ -2,13 +2,32 @@ package com.scoreboard.core.service;
 
 import com.scoreboard.core.domain.FootballMatch;
 import com.scoreboard.core.port.in.GetMatchesSummaryUseCase;
+import com.scoreboard.core.port.out.GetMatchesSummaryPort;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class GetMatchesSummaryService implements GetMatchesSummaryUseCase {
 
+    private final GetMatchesSummaryPort getMatchesSummaryPort;
+
+    public GetMatchesSummaryService(GetMatchesSummaryPort getMatchesSummaryPort) {
+        this.getMatchesSummaryPort = getMatchesSummaryPort;
+    }
+
     @Override
     public List<FootballMatch> apply() {
-        return List.of();
+
+        List<FootballMatch> allFootballMatches = getMatchesSummaryPort.apply();
+
+        return allFootballMatches.stream()
+                .filter(footballMatch -> footballMatch.getFinishedAt() == null)
+                .sorted(
+                        Comparator
+                                .comparingInt((FootballMatch m) -> m.getHomeTeamScore() + m.getAwayTeamScore())
+                                .reversed()
+                                .thenComparing(FootballMatch::getStartedAt, Comparator.reverseOrder())
+                )
+                .toList();
     }
 }
