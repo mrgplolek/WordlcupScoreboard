c97b4aeaeea76ba2a4b26313271c13c4b0795daf
Grzegorz Pelc <grzegorz.pelc.fy@gmail.com>
2025-07-06 22:07:59 +0200

    implement UpdateMatchService

--- core/application/src/main/java/com/scoreboard/core/service/UpdateMatchService.java
+++ core/application/src/main/java/com/scoreboard/core/service/UpdateMatchService.java
@@ -1,12 +1,35 @@
 package com.scoreboard.core.service;
 
 import com.scoreboard.core.domain.FootballMatch;
+import com.scoreboard.core.domain.exception.IncorrectUpdateMatchDataException;
+import com.scoreboard.core.domain.exception.MatchNotFoundException;
 import com.scoreboard.core.port.in.UpdateMatchUseCase;
+import com.scoreboard.core.port.out.FindRunningMatchByContestantsPort;
+import com.scoreboard.core.port.out.UpdateMatchPort;
 
 public class UpdateMatchService implements UpdateMatchUseCase {
 
+    private FindRunningMatchByContestantsPort findRunningMatchByContestantsPort;
+    private UpdateMatchPort updateMatchPort;
+
     @Override
     public FootballMatch apply(FootballMatch match) {
-        return null;
+        FootballMatch matchToBeUpdated = findRunningMatchByContestantsPort.apply(match.getHomeTeam(), match.getAwayTeam());
+        if (matchToBeUpdated == null) {
+            throw new MatchNotFoundException("There is no currently running match between %s and %s."
+                    .formatted(match.getHomeTeam(), match.getAwayTeam()));
+        }
+        if (match.getAwayTeamScore() < 0 || match.getHomeTeamScore() < 0){
+            throw new IncorrectUpdateMatchDataException("Goal data cannot contain negative numbers.");
+        }
+        if (isUpdatingMoreThanOneGoal(match, matchToBeUpdated)){
+            throw new IncorrectUpdateMatchDataException("Cannot update more than one goal.");
+        }
+        return updateMatchPort.apply(match);
+    }
+
+    private boolean isUpdatingMoreThanOneGoal(FootballMatch match, FootballMatch matchToBeUpdated) {
+        return (match.getHomeTeamScore() != matchToBeUpdated.getHomeTeamScore() && match.getAwayTeamScore() != matchToBeUpdated.getAwayTeamScore())
+                || Math.abs(matchToBeUpdated.getHomeTeamScore() - match.getHomeTeamScore()) > 1 ||  Math.abs(matchToBeUpdated.getAwayTeamScore() - match.getAwayTeamScore()) > 1;
     }
 }
