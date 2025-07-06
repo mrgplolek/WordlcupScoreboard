eaded5a19d07cb4a7007867716258b5637171c4e
Grzegorz Pelc <grzegorz.pelc.fy@gmail.com>
2025-07-06 23:43:19 +0200

    Reproduce example in main

--- bootstrap/src/main/java/com/scoreboard/bootstrap/Main.java
+++ bootstrap/src/main/java/com/scoreboard/bootstrap/Main.java
@@ -1,9 +1,68 @@
 package com.scoreboard.bootstrap;
 
 
+import com.scoreboard.adapter.inweb.ScoreboardController;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+    public static void main(String[] args) throws InterruptedException {
+
+
+        ScoreboardController controller = ScoreboardController.getInstance();
+
+        //Setup data
+        controller.startMatch("Mexico", "Canada");
+        controller.updateMatch("Mexico", "Canada", 0, 1);
+        controller.updateMatch("Mexico", "Canada", 0, 2);
+        controller.updateMatch("Mexico", "Canada", 0, 3);
+        controller.updateMatch("Mexico", "Canada", 0, 4);
+        controller.updateMatch("Mexico", "Canada", 0, 5);
+        Thread.sleep(1000);
+
+        controller.startMatch("Spain", "Brazil");
+        controller.updateMatch("Spain", "Brazil", 0, 1);
+        controller.updateMatch("Spain", "Brazil", 0, 2);
+        controller.updateMatch("Spain", "Brazil", 1, 2);
+        controller.updateMatch("Spain", "Brazil", 2, 2);
+        controller.updateMatch("Spain", "Brazil", 3, 2);
+        controller.updateMatch("Spain", "Brazil", 4, 2);
+        controller.updateMatch("Spain", "Brazil", 5, 2);
+        controller.updateMatch("Spain", "Brazil", 6, 2);
+        controller.updateMatch("Spain", "Brazil", 7, 2);
+        controller.updateMatch("Spain", "Brazil", 8, 2);
+        controller.updateMatch("Spain", "Brazil", 9, 2);
+        controller.updateMatch("Spain", "Brazil", 10, 2);
+        Thread.sleep(1000);
+
+        controller.startMatch("Germany", "France");
+        controller.updateMatch("Germany", "France", 0, 1);
+        controller.updateMatch("Germany", "France", 1, 1);
+        controller.updateMatch("Germany", "France", 2, 1);
+        controller.updateMatch("Germany", "France", 2, 2);
+        Thread.sleep(1000);
+
+        controller.startMatch("Uruguay", "Italy");
+        controller.updateMatch("Uruguay", "Italy", 0, 1);
+        controller.updateMatch("Uruguay", "Italy", 0, 2);
+        controller.updateMatch("Uruguay", "Italy", 0, 3);
+        controller.updateMatch("Uruguay", "Italy", 1, 3);
+        controller.updateMatch("Uruguay", "Italy", 2, 3);
+        controller.updateMatch("Uruguay", "Italy", 3, 3);
+        controller.updateMatch("Uruguay", "Italy", 4, 3);
+        controller.updateMatch("Uruguay", "Italy", 5, 3);
+        controller.updateMatch("Uruguay", "Italy", 6, 3);
+        controller.updateMatch("Uruguay", "Italy", 6, 4);
+        controller.updateMatch("Uruguay", "Italy", 6, 5);
+        controller.updateMatch("Uruguay", "Italy", 6, 6);
+        Thread.sleep(1000);
+
+        controller.startMatch("Argentina", "Australia");
+        controller.updateMatch("Argentina", "Australia", 0, 1);
+        controller.updateMatch("Argentina", "Australia", 1, 1);
+        controller.updateMatch("Argentina", "Australia", 2, 1);
+        controller.updateMatch("Argentina", "Australia", 3, 1);
+        Thread.sleep(1000);
+
+        controller.getSummary();
         }
 }
